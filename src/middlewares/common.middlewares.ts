8ab76643609680a47a4ca8ea6b94ebb5272f7933
EmTanIT <tan182205@gmail.com>
2023-12-10 00:37:18 +0700

    filter body middlewares

--- src/middlewares/common.middlewares.ts
+++ src/middlewares/common.middlewares.ts
@@ -1,6 +1,7 @@
 import { Response, Request, NextFunction } from 'express'
 import { pick } from 'lodash'
-//ta đang dùng generic để khi dùng hàm filterMiddleware nó sẽ nhắc ta nên bỏ property nào vào mảng
+//ta đang dùng generic để khi dùng hàm filterMiddleware
+//nó sẽ nhắc ta nên bỏ property nào vào mảng
 //FilterKeys là mảng các key của object T nào đó
 type FilterKeys<T> = Array<keyof T>
 
