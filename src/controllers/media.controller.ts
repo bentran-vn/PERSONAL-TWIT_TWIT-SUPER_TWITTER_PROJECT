ebcc57a5b6f4b1088a29c95151730baa7f870308
EmTanIT <tan182205@gmail.com>
2023-12-11 22:35:58 +0700

    Sharp Initialization

--- src/controllers/media.controller.ts
+++ src/controllers/media.controller.ts
@@ -1,8 +1,4 @@
 import { Request, Response } from 'express'
-import formidable from 'formidable'
-import path from 'path'
-import HTTP_STATUS from '~/constants/httpStatus'
-import { ErrorWithStatus } from '~/models/Error'
 import { handleUploadSingleImage } from '~/utils/file'
 
 export const uploadSingleImageController = async (req: Request, res: Response) => {
